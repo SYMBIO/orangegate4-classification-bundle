725942b07b1a472959f8de49ec23a8c5e81f4a54
Tomáš Vítek <tomas.vitek@symbio.cz>
2015-08-19 14:32:32 +0200

    categories load should respect position settings

--- Entity/CategoryManager.php
+++ Entity/CategoryManager.php
@@ -121,4 +121,58 @@ class CategoryManager extends \Sonata\ClassificationBundle\Entity\CategoryManage
 
         return $pager;
     }
+
+    /**
+     * {@inheritdoc}
+     * @Order by category position
+     */
+    protected function loadCategories(ContextInterface $context)
+    {
+        if (array_key_exists($context->getId(), $this->categories)) {
+            return;
+        }
+
+        $class = $this->getClass();
+
+        $categories = $this->getObjectManager()->createQuery(sprintf('SELECT c FROM %s c WHERE c.context = :context ORDER BY c.parent ASC, c.position ASC', $class))
+            ->setParameter('context', $context->getId())
+            ->execute();
+
+        if (count($categories) == 0) {
+            // no category, create one for the provided context
+            $category = $this->create();
+            $category->setName($context->getName());
+            $category->setEnabled(true);
+            $category->setContext($context);
+            $category->setDescription($context->getName());
+
+            $this->save($category);
+
+            $categories = array($category);
+        }
+
+        foreach ($categories as $pos => $category) {
+            if ($pos === 0 && $category->getParent()) {
+                throw new \RuntimeException('The first category must be the root');
+            }
+
+            if ($pos == 0) {
+                $root = $category;
+            }
+
+            $this->categories[$context->getId()][$category->getId()] = $category;
+
+            $parent = $category->getParent();
+
+            $category->disableChildrenLazyLoading();
+
+            if ($parent) {
+                $parent->addChild($category);
+            }
+        }
+
+        $this->categories[$context->getId()] = array(
+            0 => $root
+        );
+    }
 }
